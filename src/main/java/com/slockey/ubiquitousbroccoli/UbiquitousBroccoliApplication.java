3a5d8506518275eec06f2163a191fc4a574e5f8c
Steven Lockey <steven.lockey@cision.com>
2022-12-02 10:55:59 -0500

    First commit of framing/boilerplate project code + spring boot v. 2.7.6 + java v. 11 + maven wrapper + default application, test and properties file

---
+++ src/main/java/com/slockey/ubiquitousbroccoli/UbiquitousBroccoliApplication.java
@@ -0,0 +1,13 @@
+package com.slockey.ubiquitousbroccoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbiquitousBroccoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbiquitousBroccoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slockey/ubiquitousbroccoli/UbiquitousBroccoliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slockey.ubiquitousbroccoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbiquitousBroccoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
